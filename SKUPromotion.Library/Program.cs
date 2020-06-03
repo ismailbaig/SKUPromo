30d8fa2589be45b320cb2c8aa6e6211223f2bd85
ismailbaig <ismailece2007@gmail.com>
2020-06-03 06:19:16 +0530

    Refactor to make sure athe first dummy test works

---
+++ SKUPromotion.Library/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKUPromotion.Library
+{
+
+    public class SKUPromotions
+    {
+        public double Add(double ATotalUnits, double BTotalUnits, double CTotalUnits, double DTotalUnits)
+        {
+            return 4;
+        }
+    }
+
+        class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ SKUPromotions.Tests/SKUPromoCalcTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SKUPromotions.Tests
+{
+    [TestClass]
+    public class SKUPromoCalcTest
+    {
+
+        private readonly SKUPromotion.Library.SKUPromotions _sut_skuPromos;
+
+        public SKUPromoCalcTest()
+        {
+            _sut_skuPromos = new SKUPromotion.Library.SKUPromotions();
+        }
+
+        [TestMethod]
+        public void DummyTestToCheckIfRefactorWorked()
+        {
+            //Arrange
+            double AItems = 1, BItems = 1, CItems = 1, DItems = 1, expected = 4, actual = 0;
+
+            //Act
+            actual = _sut_skuPromos.Add(AItems, BItems, CItems, DItems);
+
+            //Assert            
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
