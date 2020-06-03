104335bcc2a0dd101c904fe4868a5b502c8eb134
ismailbaig <ismailece2007@gmail.com>
2020-06-03 08:14:21 +0530

    Util file was missed to checkout previously, Adding now

---
+++ SKUPromotion.Library/Util.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKUPromotion.Library
+{
+    public static class Util
+    {
+       static double _totalAmount = 0, totalPromtionItems = 0, totalNonPromoItems = 0;
+
+        public static double CalculatePromoPriceForSingleItem(double totalUnits, int unitForPromoItems, int PromoPrice, int nonPromoPrice)
+        {
+            try
+            {
+                totalPromtionItems = Math.Floor(totalUnits / (int)(unitForPromoItems));
+                totalNonPromoItems = totalUnits % (int)(unitForPromoItems);
+
+                return totalPromtionItems * (int)(PromoPrice) + totalNonPromoItems * (int)(nonPromoPrice);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+    }
+}
