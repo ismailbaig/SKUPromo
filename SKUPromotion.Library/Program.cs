d561cbf99ae487d20398c47fa1c8655011a1f3ac
ismailbaig <ismailece2007@gmail.com>
2020-06-03 08:33:21 +0530

    Refator for better naming conventions

--- SKUPromotion.Library/Program.cs
+++ SKUPromotion.Library/Program.cs
@@ -8,15 +8,15 @@ namespace SKUPromotion.Library
 {
     enum SKUUnitPrices : int
     {
-        ANonPromoPrice = 50,
-        BNonPromoPrice = 30,
-        CNonPromoPrice = 20,
-        DNonPromoPrice = 15,
+        A_SKU_Non_Promo_Price = 50,
+        B_SKU_Non_Promo_Price = 30,
+        C_SKU_Non_Promo_Price = 20,
+        D_SKU_Non_Promo_Price = 15,
         A_SKUs_To_Promo = 3,
         B_SKUs_To_Promo = 2,
-        APromoPrice = 130,
-        BPromoPrice = 45,
-        CnDPromoPirce = 30,
+        A_SKU_Promo_Price = 130,
+        B_SKU_Promo_Price = 45,
+        CnD_SKU_Promo_Pirce = 30,
     }
 
     public class SKUPromotions
@@ -29,20 +29,20 @@ namespace SKUPromotion.Library
             {
 
                 #region A Promotion
-                _totalAmount += Util.CalculatePromoPriceForSingleItem(ATotalUnits, (int)SKUUnitPrices.A_SKUs_To_Promo, (int)SKUUnitPrices.APromoPrice, (int)SKUUnitPrices.ANonPromoPrice);
+                _totalAmount += Util.CalculatePromoPriceForSingleItem(ATotalUnits, (int)SKUUnitPrices.A_SKUs_To_Promo, (int)SKUUnitPrices.A_SKU_Promo_Price, (int)SKUUnitPrices.A_SKU_Non_Promo_Price);
                 #endregion
 
                 #region B Promotion 
-                _totalAmount += Util.CalculatePromoPriceForSingleItem(BTotalUnits, (int)SKUUnitPrices.B_SKUs_To_Promo, (int)SKUUnitPrices.BPromoPrice, (int)SKUUnitPrices.BNonPromoPrice);
+                _totalAmount += Util.CalculatePromoPriceForSingleItem(BTotalUnits, (int)SKUUnitPrices.B_SKUs_To_Promo, (int)SKUUnitPrices.B_SKU_Promo_Price, (int)SKUUnitPrices.B_SKU_Non_Promo_Price);
                 #endregion
 
                 #region C n D Promotion
-                _totalAmount += Math.Min(CTotalUnits, DTotalUnits) * (int)SKUUnitPrices.CnDPromoPirce;
+                _totalAmount += Math.Min(CTotalUnits, DTotalUnits) * (int)SKUUnitPrices.CnD_SKU_Promo_Pirce;
                 #endregion
 
                 #region C n D Left out Logic
-                _totalAmount += (CTotalUnits - DTotalUnits) > 0 ? Math.Abs(CTotalUnits - DTotalUnits) * (int)(SKUUnitPrices.CNonPromoPrice) :
-                                                                    Math.Abs(CTotalUnits - DTotalUnits) * (int)(SKUUnitPrices.DNonPromoPrice);
+                _totalAmount += (CTotalUnits - DTotalUnits) > 0 ? Math.Abs(CTotalUnits - DTotalUnits) * (int)(SKUUnitPrices.C_SKU_Non_Promo_Price) :
+                                                                    Math.Abs(CTotalUnits - DTotalUnits) * (int)(SKUUnitPrices.D_SKU_Non_Promo_Price);
                 #endregion
 
                 return _totalAmount;
